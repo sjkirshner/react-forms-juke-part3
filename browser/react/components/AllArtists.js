2c707fd546255abd20f43513634f3ff1447c7303
Robin Wilson <robin.kath.wilson@gmail.com>
2017-08-23 15:14:53 -0700

    making the filter things work

--- browser/react/components/AllArtists.js
+++ browser/react/components/AllArtists.js
@@ -7,8 +7,10 @@ export default class AllArtists extends Component {
   constructor () {
     super();
     this.state = {
-      artists: []
+      artists: [],
+      input: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount () {
@@ -17,16 +19,31 @@ export default class AllArtists extends Component {
       .then(artists => this.setState({ artists }));
   }
 
+  handleChange (event) {
+    console.log(event.target.value);
+
+    this.setState({ input: event.target.value });
+  }
+
   render () {
 
-    const artists = this.state.artists;
+    const artists = this.state.input === '' 
+    ? this.state.artists 
+    : this.state.artists.filter(artist => artist.name.match(this.state.input))
 
     return (
       <div>
         <h3>Artists</h3>
+        <form onChange={this.handleChange} className="form-group" style={{marginTop: '20px'}}>
+          <input
+            className="form-control"
+            placeholder="Enter artist name"   
+          />
+        </form>
         <div className="list-group">
-          {
-            artists.map(artist => {
+
+        { 
+          artists.map(artist => {
               return (
                 <div className="list-group-item" key={artist.id}>
                   <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
@@ -39,3 +56,17 @@ export default class AllArtists extends Component {
     );
   }
 }
+
+
+
+/*
+
+            artists.map(artist => {
+              return (
+                <div className="list-group-item" key={artist.id}>
+                  <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
+                </div>
+              );
+            })
+
+*/
